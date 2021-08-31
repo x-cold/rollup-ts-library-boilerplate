548fec4cffd59bd0a813dc5f8a5ae028bcea7e07
x-cold <lxstart@outlook.com>
2021-08-31 11:52:49 +0800

    chore: first commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+}
