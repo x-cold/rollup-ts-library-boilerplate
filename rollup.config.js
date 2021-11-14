ab9707dccc118741f67803f98ecc2f5d1d4dcc77
x-cold <lxstart@outlook.com>
2021-11-14 12:22:22 +0800

    docs: node version should >= 12

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
+import { babel } from '@rollup/plugin-babel';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('./package.json');
@@ -27,6 +28,7 @@ export default {
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
+    babel({ babelHelpers: 'bundled' }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
